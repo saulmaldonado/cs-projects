07bf5f11b303082bba5d58dfd8f3cdf0eee0d072
Saul Maldonado <samaldonado4@gmail.com>
2020-04-24 10:56:20 -0500

    inital commit, added posts project files and gitignore.

---
+++ cs-post/cs-post/Post.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StackOverflow_Post
+{
+    class Post
+    {
+        public string title { get; private set; }
+        public string description { get; private set; }
+        public DateTime date { get; private set; }
+        public int score { get; private set; } = 0;
+
+        public Post()
+        {
+            Console.WriteLine("Enter a title for your post... ");
+            title = Console.ReadLine();
+            Console.WriteLine("Enter a description for your post... ");
+            description = Console.ReadLine();
+            date = DateTime.Now;
+
+            Console.WriteLine(info());
+        }
+
+        public string upvote()
+        {
+            score += 1;
+            return score.ToString();
+        }
+
+        public string downvote()
+        {
+            score -= 1;
+            return score.ToString();
+        }
+
+        public string info()
+        {
+            return string.Format("Titie: {0}\n Description: {1}\n Time posted: {2}\n Score: {3}", title, description, date.ToShortTimeString(), score);
+        }
+
+        
+
+
+
+
+    }
+}
---
+++ cs-post/cs-post/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace StackOverflow_Post
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input;
+            Post post = new Post();
+
+            while (true)
+            {
+
+                Console.WriteLine("type \"info\" to display post information\ntype \"upvote\" to upvote the post\ntype \"downvote\" to downvote the post\ntype \"exit\" to exit the program ");
+                input = Console.ReadLine();
+
+
+                switch (input.ToLower())
+                {
+                    case "info":
+                        Console.WriteLine(post.info());
+                        break;
+
+                    case "upvote":
+                        Console.WriteLine(post.upvote());
+                        break;
+                    case "downvote":
+                        Console.WriteLine(post.downvote());
+                        break;
+                    case "exit":
+                        return;
+                    default:
+                        break;
+                }
+
+                
+            }
+        }
+    }
+}
